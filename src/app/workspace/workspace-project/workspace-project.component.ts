d6c5237dd829ac59bc5fa5d87200279bcd218ad2
Hamza <hm.khanzada786@gmail.com>
2020-02-15 14:53:24 +0500

    minor fixes

--- src/app/workspace/workspace-project/workspace-project.component.ts
+++ src/app/workspace/workspace-project/workspace-project.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-workspace-project',
-  templateUrl: './workspace-project.component.html',
-  styleUrls: ['./workspace-project.component.css']
+  selector: "app-workspace-project",
+  templateUrl: "./workspace-project.component.html",
+  styleUrls: ["./workspace-project.component.css"]
 })
 export class WorkspaceProjectComponent implements OnInit {
+  projects = [
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    },
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    },
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    },
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    },
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    },
+    {
+      project_name: "Locker app",
+      description: "A locker app for android smartphones",
+      project_manager: "Hamza Mushtaq",
+      start_date: "15/02/2020",
+      end_date: "20/5/2020",
+      status: "In progress"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class WorkspaceComponent implements OnInit {
   screen = "workspace_home";
-
+  page = "Home";
   constructor() {}
 
   ngOnInit() {}
@@ -15,12 +15,16 @@ export class WorkspaceComponent implements OnInit {
   leftbar_click(event) {
     if (event.target.innerText === "Home") {
       this.screen = "workspace_home";
+      this.page = "Home";
     } else if (event.target.innerText === "Feed") {
       this.screen = "feed";
+      this.page = "Feed";
     } else if (event.target.innerText === "Projects") {
       this.screen = "workspace_project";
+      this.page = "Projects";
     } else if (event.target.innerText === "Files") {
       this.screen = "workspace_files";
+      this.page = "Files";
     }
   }
 }
