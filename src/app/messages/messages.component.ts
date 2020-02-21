2ec9ff95e5465aad518a3c55e4fc65f6bb15ba67
Hamza <hm.khanzada786@gmail.com>
2020-02-21 13:33:05 +0500

    messages module left side frontend completed

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,153 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "app-messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"]
 })
 export class MessagesComponent implements OnInit {
+  messages = [
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    },
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    },
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    },
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    },
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    },
+    {
+      first_name: "Hamza",
+      last_name: "Mushtaq",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "Hey text me when you get back we need to work on this project soon."
+    },
+    {
+      first_name: "Umar",
+      last_name: "Shah",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body:
+        "It was so nice to meet you. Let's catch up again sometime. See you."
+    },
+    {
+      first_name: "Iqra",
+      last_name: "Khan",
+      photo_address:
+        "https://image.freepik.com/free-vector/businessman-character-avatar-icon-vector-illustration-design_24877-18271.jpg",
+      message_body: "That's great!"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
