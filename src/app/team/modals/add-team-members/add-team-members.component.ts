42174838d1e5d52dec380e1d38491ebf42c340b5
Hamza <hm.khanzada786@gmail.com>
2020-03-03 07:58:58 +0500

    team module from arzoo with few bugs

--- src/app/team/modals/add-team-members/add-team-members.component.ts
+++ src/app/team/modals/add-team-members/add-team-members.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-team-members',
-  templateUrl: './add-team-members.component.html',
-  styleUrls: ['./add-team-members.component.css']
+  selector: "app-add-team-members",
+  templateUrl: "./add-team-members.component.html",
+  styleUrls: ["./add-team-members.component.css"]
 })
 export class AddTeamMembersComponent implements OnInit {
+  members = [
+    {
+      name: "Hamza Mushtaq",
+      email: "hm.khanzada786@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Ali Mushtaq",
+      email: "ali.mushtaq@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Basit Ahmed",
+      email: "ahmed.basit@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Aisha Khan",
+      email: "khan.aisha@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Amjad Dawood",
+      email: "amjad.dawood@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Ahmer Raza",
+      email: "ahmer.raza@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Hamza Mushtaq",
+      email: "hm.khanzada786@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Ali Mushtaq",
+      email: "ali.mushtaq@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Basit Ahmed",
+      email: "ahmed.basit@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Aisha Khan",
+      email: "khan.aisha@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Amjad Dawood",
+      email: "amjad.dawood@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    },
+    {
+      name: "Ahmer Raza",
+      email: "ahmer.raza@gmail.com",
+      image: "https://mdbootstrap.com/img/Photos/Avatars/img%20(27).jpg"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
