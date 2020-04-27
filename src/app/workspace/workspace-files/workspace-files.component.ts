236dc6826faef2b432c9c745fbf20ddd0c65b821
Hamza <hm.khanzada786@gmail.com>
2020-04-27 15:47:31 +0500

    workspaces files done

--- src/app/workspace/workspace-files/workspace-files.component.ts
+++ src/app/workspace/workspace-files/workspace-files.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { ConnectionService } from "src/app/connection.service";
 
 declare const fileTable: any;
 
 @Component({
   selector: "app-workspace-files",
   templateUrl: "./workspace-files.component.html",
-  styleUrls: ["./workspace-files.component.css"]
+  styleUrls: ["./workspace-files.component.css"],
 })
 export class WorkspaceFilesComponent implements OnInit {
   files = [
@@ -14,69 +15,88 @@ export class WorkspaceFilesComponent implements OnInit {
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly report.xlsx",
       comment: "Yearly report for the year 2019",
       author: "Hamza Mushtaq",
       ext: ".xlsx",
-      time: "12:51 15/2/2020"
+      time: "12:51 15/2/2020",
     },
     {
       file_name: "Yearly expenses.docx",
       comment: "for the year 2019",
       author: "Abid Mushtaq",
       ext: ".docx",
-      time: "13:11 13/1/2020"
-    }
+      time: "13:11 13/1/2020",
+    },
   ];
 
-  constructor() {}
+  @Input() currentWorkspace;
+
+  filesCount = 0;
+  workspaceFiles = new Array();
+  constructor(private connectionService: ConnectionService) {}
 
   ngOnInit() {
     fileTable();
+    this.connectionService
+      .getWorkspacePostsByWID(this.currentWorkspace.w_id)
+      .subscribe(
+        (GetWorkspacePostsByWIDResult: any) => {
+          GetWorkspacePostsByWIDResult.forEach((element) => {
+            if (element.pst_filepath) {
+              this.workspaceFiles.push(element);
+              this.filesCount++;
+            }
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
