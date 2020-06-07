0e5ca8ef0502c94a4d864ce9911a2dc025bc1a63
Hamza <hm.khanzada786@gmail.com>
2020-06-07 16:17:06 +0500

    project feed side members area done

--- src/app/project/project-feed/project-feed.component.ts
+++ src/app/project/project-feed/project-feed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { ConnectionService } from "src/app/connection.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-project-feed",
@@ -10,45 +13,143 @@ export class ProjectFeedComponent implements OnInit {
 
   user: any;
   org: any;
+  projMembers: any;
 
   tempUsers: any;
 
-  constructor() {}
+  userPostsData: any;
+
+  postForm = new FormGroup({
+    postContent: new FormControl(),
+  });
+
+  commentForm = new FormGroup({
+    commentContent: new FormControl(),
+    commentPostID: new FormControl(),
+  });
+
+  postObj = {
+    pst_content: "",
+    created_by: "",
+    pst_filepath: "",
+    project_id: "",
+    pst_filename: "",
+  };
+
+  commentObj = {
+    c_content: "",
+    created_by: "",
+    post_id: "",
+  };
+
+  constructor(
+    private connectionService: ConnectionService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem("user"));
     this.org = JSON.parse(localStorage.getItem("org"));
-    this.tempUsers = [
-      {
-        first_name: "Hamza",
-        last_name: "Mushtaq",
-        email: "somemeail@gmail.com",
-        photo_address: this.user.photo_address,
-      },
-      {
-        first_name: "Umer",
-        last_name: "Khan",
-        email: "somemeail@gmail.com",
-        photo_address: this.user.photo_address,
-      },
-      {
-        first_name: "Ahsan",
-        last_name: "Ali",
-        email: "somemeail@gmail.com",
-        photo_address: this.user.photo_address,
-      },
-      {
-        first_name: "Adil",
-        last_name: "Shiekh",
-        email: "somemeail@gmail.com",
-        photo_address: this.user.photo_address,
-      },
-      {
-        first_name: "Iqbal",
-        last_name: "Raza",
-        email: "somemeail@gmail.com",
-        photo_address: this.user.photo_address,
-      },
-    ];
+    // this.members = JSON.parse(localStorage.getItem("workspace-members"));
+
+    // this.getPosts();
+
+    this.connectionService
+      .getTotalProjects("p" + this.currentProject.p_id__p_id)
+      .subscribe(
+        (getTotalProjectsResult: any) => {
+          this.projMembers = getTotalProjectsResult;
+          console.log(this.projMembers);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
+
+  // createPost() {
+  //   $("#workspacePostBtn")
+  //     .html(
+  //       '<span class="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>'
+  //     )
+  //     .addClass("disabled");
+  //   this.postObj.pst_content = this.postForm.get("postContent").value;
+  //   this.postObj.created_by = this.user.id;
+  //   this.postObj.workspace_id = this.currentWorkspace.w_id;
+  //   this.postObj.pst_filename = this.postObj.pst_filepath["name"];
+
+  //   this.connectionService
+  //     .uploadWorkspaceFile(this.postObj.pst_filepath)
+  //     .subscribe(
+  //       (UploadWorkspaceFileResult: any) => {
+  //         this.postObj.pst_filepath = UploadWorkspaceFileResult.secure_url;
+  //         this.connectionService.createWorkspacePost(this.postObj).subscribe(
+  //           (CreateWorkspacePostResult: any) => {
+  //             this.router
+  //               .navigateByUrl("/loading", { skipLocationChange: true })
+  //               .then(() => {
+  //                 this.router.navigate([
+  //                   "workspace/" + this.currentWorkspace.w_id,
+  //                 ]);
+  //               });
+  //           },
+  //           (error) => {
+  //             console.log(error);
+  //           }
+  //         );
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  // }
+
+  // userFileSelected = (event) => {
+  //   this.postObj.pst_filepath = event.target.files[0];
+  // };
+
+  // getPosts() {
+  //   this.connectionService
+  //     .getWorkspacePostsByWID(this.currentWorkspace.w_id)
+  //     .subscribe(
+  //       (GetWorkspacePostsByWIDResult: any) => {
+  //         this.userPostsData = GetWorkspacePostsByWIDResult;
+  //         this.getComments();
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  // }
+
+  // getComments() {
+  //   this.userPostsData.forEach((post) => {
+  //     this.connectionService.getWorkspaceCommentByPID(post.pst_id).subscribe(
+  //       (GetWorkspaceCommentByPIDResult: any) => {
+  //         post["commentData"] = GetWorkspaceCommentByPIDResult;
+  //       },
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     );
+  //   });
+  // }
+
+  // createComment(id) {
+  //   this.commentObj.c_content = this.commentForm.get("commentContent").value;
+  //   this.commentObj.created_by = this.user.id;
+  //   this.commentObj.post_id = id;
+
+  //   this.connectionService.createWorkspaceComment(this.commentObj).subscribe(
+  //     (CreateWorkspaceCommentResult: any) => {
+  //       console.log(CreateWorkspaceCommentResult);
+  //       this.router
+  //         .navigateByUrl("/loading", { skipLocationChange: true })
+  //         .then(() => {
+  //           this.router.navigate(["workspace/" + this.currentWorkspace.w_id]);
+  //         });
+  //     },
+  //     (error) => console.log(error)
+  //   );
+  // }
 }
