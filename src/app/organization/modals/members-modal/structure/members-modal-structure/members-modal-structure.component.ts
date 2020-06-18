a2de506aeb79e7a8864aef0334be6a4c5ea43ac2
Hamza <hm.khanzada786@gmail.com>
2020-06-18 16:08:57 +0500

    message link from organization added

--- src/app/organization/modals/members-modal/structure/members-modal-structure/members-modal-structure.component.ts
+++ src/app/organization/modals/members-modal/structure/members-modal-structure/members-modal-structure.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-members-modal-structure",
   templateUrl: "./members-modal-structure.component.html",
-  styleUrls: ["./members-modal-structure.component.css"]
+  styleUrls: ["./members-modal-structure.component.css"],
 })
 export class MembersModalStructureComponent implements OnInit {
   orgMembers: any;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.orgMembers = JSON.parse(localStorage.getItem("org-members"));
+  }
+
+  navigateToMessages() {
+    var membersModal: any = $("#membersModal");
+    membersModal.modal("hide");
+    this.router.navigate(["messages/"]);
   }
 }
