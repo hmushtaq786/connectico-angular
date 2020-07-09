fe5d9654bd6b30ef7a94d7db0d8eb5e21445045b
Hamza <hm.khanzada786@gmail.com>
2020-07-09 18:25:55 +0500

    few changes

--- src/app/project/modals/completed-team/completed-team.component.ts
+++ src/app/project/modals/completed-team/completed-team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-completed-team",
@@ -8,9 +9,38 @@ import { Component, OnInit, Input } from "@angular/core";
 export class CompletedTeamComponent implements OnInit {
   @Input() currentTeam;
   teamName;
-  constructor() {}
+  team_lead = {
+    id: 0,
+    photo_address: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+  };
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.teamName = this.currentTeam.tm_name;
+  }
+
+  getDate(date) {
+    date = new Date(Date.parse(date));
+
+    return date.getDate();
+  }
+  getMonth(date) {
+    date = new Date(Date.parse(date));
+
+    return date.getMonth();
+  }
+  getYear(date) {
+    date = new Date(Date.parse(date));
+
+    return date.getFullYear();
+  }
+
+  navigateToMessages() {
+    var membersModal: any = $("#inProgressTeam");
+    membersModal.modal("hide");
+    this.router.navigate(["messages/"]);
   }
 }
